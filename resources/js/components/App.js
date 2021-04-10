c5a339d77ae85725d2342ef92b9ccbd3388d7b64
afifwijaya16 <afif0071997@gmail.com>
2021-04-10 16:41:41 +0700

    inc babelrc

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-    return (
-        <div className="container">
-            <h1>Hello</h1>
-        </div>
-    );
+class App extends Component {
+    state = {
+        counter: 0
+    };
+
+    incrementCounter = () => {
+        let counter = this.state.counter + 1;
+        this.setState({
+            counter
+        });
+    };
+
+    decrementCounter = () => {
+        let counter = this.state.counter - 1;
+        this.setState({
+            counter
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="container mt-5">
+                    <h2>Count: {this.state.counter}</h2>
+                    <p>
+                        <button
+                            className="btn btn-success btn-lg"
+                            onClick={() => this.incrementCounter()}
+                        >
+                            +
+                        </button>
+
+                        <button
+                            className="btn btn-danger btn-lg ml-2"
+                            onClick={() => this.decrementCounter()}
+                        >
+                            -
+                        </button>
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
